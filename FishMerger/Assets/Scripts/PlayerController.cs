52ef51e280d7f9d33ec5a0e99d907253914ca562
v.svistunov <v.svistunov@corp.nekki.ru>
2024-05-25 13:59:38 +0300

    сила импульса

--- FishMerger/Assets/Scripts/PlayerController.cs
+++ FishMerger/Assets/Scripts/PlayerController.cs
@@ -1,44 +1,47 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float impulseForceOnce = 5f; // Сила импульса при одном нажатии
-    public float impulseForceMin = 1f; // Минимальная сила импульса при удерживании
-    public float impulseForceMax = 10f; // Максимальная сила импульса при удерживании
-    public float forceTime = 4f; // Время, за которое сила импульса увеличивается от минимальной до максимальной
+    public float impulseForceMin = 1f;
+    public float impulseForceMax = 10f;
+    public float forceTime = 4f;
 
-    private Vector3 impulseDirection = new Vector3(1, 1, 0); // Направление импульса
-    private bool isPressing = false; // Флаг для отслеживания удержания клавиши пробела
-    private float pressStartTime; // Время начала удерживания клавиши пробела
+    private Vector3 impulseDirection = new Vector3(1, 1, 0);
+    private bool isPressing = false;
+    private float pressStartTime;
     public Player player;
+    public HealthBar forceSlider;
+
+    private float currentForce = 0f;
+
+    public void Start()
+    {
+        forceSlider.SetValues(impulseForceMin, impulseForceMax);
+    }
 
     void Update()
     {
-        // Проверка нажатия клавиши пробела
         if (Input.GetKeyDown(KeyCode.Space))
         {
             isPressing = true;
             pressStartTime = Time.time;
         }
 
-        // Проверка отпускания клавиши пробела
+        if (isPressing && Input.GetKey(KeyCode.Space))
+        {
+            float heldTime = Time.time - pressStartTime;
+            float t = Mathf.Clamp01(heldTime / forceTime);
+            currentForce = Mathf.Lerp(impulseForceMin, impulseForceMax, t);
+            forceSlider.SetForce(currentForce);
+        }
+
         if (Input.GetKeyUp(KeyCode.Space))
         {
             isPressing = false;
-            float heldTime = Time.time - pressStartTime;
-            float t = Mathf.Clamp01(heldTime / forceTime);
-            float currentImpulseForce = Mathf.Lerp(impulseForceMin, impulseForceMax, t);
-            player.Push(impulseDirection * currentImpulseForce);
+            player.Push(impulseDirection * currentForce);
+            currentForce = impulseForceMin;
+            forceSlider.SetForce(currentForce);
         }
-
-       
-    }
-
-    void Push(Vector3 impulse)
-    {
-        // Ваш код для применения импульса к объекту
-        // Например, если это Rigidbody, то:
-        // GetComponent<Rigidbody>().AddForce(impulse, ForceMode.Impulse);
-        Debug.Log("Impulse applied: " + impulse);
     }
 }
